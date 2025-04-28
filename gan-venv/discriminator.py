083cd912bb59ac4cb507882382083d7aa009c992
Roshan <115006790+roshan-nahsor@users.noreply.github.com>
2025-04-28 22:52:46 +0530

    gan

---
+++ gan-venv/discriminator.py
@@ -0,0 +1,18 @@
+# discriminator.py
+import torch
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self, input_dim):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.model(x)
---
+++ gan-venv/generator.py
@@ -0,0 +1,17 @@
+# generator.py
+import torch
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, latent_dim, output_dim):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(latent_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, output_dim),
+        )
+
+    def forward(self, z):
+        return self.model(z)
