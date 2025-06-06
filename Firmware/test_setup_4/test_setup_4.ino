349b8539704cbf887b9112ff4f2be754a796617e
Roshan 1A <roshanb1943@gmail.com>
2025-06-06 15:06:46 +0530

    feat: data is logging

--- Firmware/test_setup_4/test_setup_4.ino
+++ Firmware/test_setup_4/test_setup_4.ino
@@ -6,7 +6,9 @@
 // Use 1-byte page buffer mode for low RAM on Nano
 // U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
-U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE); 	      
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);      
+// U8X8_SH1107_64X128_SW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);      
+
 
 
 const int SD_CS = 10;
@@ -26,7 +28,7 @@ void setup() {
   Serial.println("Initializing SD card...");
   if (!SD.begin(SD_CS)) {
     Serial.println("SD initialization failed!");
-    while (1); // Halt here if SD init fails
+    // while (1); // Halt here if SD init fails
   }
   Serial.println("SD initialized successfully.");
 
@@ -59,8 +61,8 @@ void loop() {
 
     u8x8.setFont(u8x8_font_chroma48medium8_r);
     u8x8.drawString(0,1,"Hello World!");
-    u8x8.setInverseFont(1);
-    u8x8.drawString(0,0,"012345678901234567890123456789");
+    // u8x8.setInverseFont(1);
+    u8x8.drawString(0,0,"0123456789");
     u8x8.setInverseFont(0);
     //u8x8.drawString(0,8,"Line 8");
     //u8x8.drawString(0,9,"Line 9");
@@ -70,7 +72,8 @@ void loop() {
   delay(1000);
 
   String dataString = "";
-    dataString =    "step,speed,c;";
+//              mode spd c    ci   co   pi   po   ei   eo   lf   rf   et   eh   ct   ch
+    dataString = "15,255,45.3,25.6,25.6,25.6,25.6,25.6,25.6,25.6,25.6,25.6,25.6,25.6,25.6;";
 
     File dataFile = SD.open("logs/f_4.csv", FILE_WRITE);
 
