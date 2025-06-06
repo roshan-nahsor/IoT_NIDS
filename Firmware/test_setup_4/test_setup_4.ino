c4e0a75b8c0e3982f442c6370847b09cd7caa405
Roshan 1A <roshanb1943@gmail.com>
2025-06-06 15:17:54 +0530

    perf: replaced drawString() with setCursor()+print()

--- Firmware/test_setup_4/test_setup_4.ino
+++ Firmware/test_setup_4/test_setup_4.ino
@@ -60,10 +60,14 @@ void loop() {
     // u8x8.setFont(u8g2_font_doomalpha04_tr);	// choose a suitable font
 
     u8x8.setFont(u8x8_font_chroma48medium8_r);
-    u8x8.drawString(0,1,"Hello World!");
+    u8x8.setCursor(0,1);
+    u8x8.print("Hello World!");
+    // u8x8.drawString(0,1,"Hello World!");
     // u8x8.setInverseFont(1);
-    u8x8.drawString(0,0,"0123456789");
-    u8x8.setInverseFont(0);
+    u8x8.setCursor(0,0);
+    // u8x8.print("Hello World!");
+    u8x8.print("0123456789");
+    // u8x8.setInverseFont(0);
     //u8x8.drawString(0,8,"Line 8");
     //u8x8.drawString(0,9,"Line 9");
     // u8x8.refreshDisplay();		// only required for SSD1606/7  
