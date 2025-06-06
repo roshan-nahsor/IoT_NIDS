c0359d921d18b2153cc3457c8f2f57ea9d7087be
Roshan 1A <roshanb1943@gmail.com>
2025-06-06 14:28:15 +0530

    u8g2 replaced with u8x8

--- Firmware/test_setup_4/test_setup_4.ino
+++ Firmware/test_setup_4/test_setup_4.ino
@@ -4,7 +4,10 @@
 #include <SD.h>
 
 // Use 1-byte page buffer mode for low RAM on Nano
-U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+// U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE); 	      
+
 
 const int SD_CS = 10;
 
@@ -29,32 +32,47 @@ void setup() {
 
   // Init I2C and OLED display
   Wire.begin();
-  u8g2.begin();
+//   u8g2.begin();
 
   // Display startup message
-  u8g2.firstPage();
-  do {
-    u8g2.setFont(u8g2_font_6x10_tr);
-    u8g2.drawStr(0, 15, "SD + OLED working");
-  } while (u8g2.nextPage());
+//   u8g2.firstPage();
+//   do {
+//     u8g2.setFont(u8g2_font_6x10_tr);
+//     u8g2.drawStr(0, 15, "SD + OLED working");
+//   } while (u8g2.nextPage());
+
+    u8x8.begin();
+    u8x8.setPowerSave(0);
 }
 
 void loop() {
   // Display running time on OLED
-  u8g2.firstPage();
-  do {
-    u8g2.setCursor(0, 20);
-    u8g2.print("Time: ");
-    u8g2.print(millis() / 1000);
-    u8g2.print(" s");
-  } while (u8g2.nextPage());
+//   u8g2.firstPage();
+//   do {
+//     u8g2.setCursor(0, 20);
+//     u8g2.print("Time: ");
+//     u8g2.print(millis() / 1000);
+//     u8g2.print(" s");
+//   } while (u8g2.nextPage());
+
+    // u8x8.setFont(u8g2_font_doomalpha04_tr);	// choose a suitable font
+
+    u8x8.setFont(u8x8_font_chroma48medium8_r);
+    u8x8.drawString(0,1,"Hello World!");
+    u8x8.setInverseFont(1);
+    u8x8.drawString(0,0,"012345678901234567890123456789");
+    u8x8.setInverseFont(0);
+    //u8x8.drawString(0,8,"Line 8");
+    //u8x8.drawString(0,9,"Line 9");
+    // u8x8.refreshDisplay();		// only required for SSD1606/7  
+    delay(2000);
 
   delay(1000);
 
   String dataString = "";
     dataString =    "step,speed,c;";
 
-    File dataFile = SD.open("logs/f_2.csv", FILE_WRITE);
+    File dataFile = SD.open("logs/f_4.csv", FILE_WRITE);
 
     // if the file is available, write to it:
     if (dataFile) {
