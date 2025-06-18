4f9d37af094c4a70a14e9e51f1d5fd8c533c553e
Roshan 1A <roshanb1943@gmail.com>
2025-06-18 14:14:45 +0530

    fix: initialised to be off

--- Firmware/test_setup_4/globals.h
+++ Firmware/test_setup_4/globals.h
@@ -7,7 +7,7 @@
 
 #define compressor_sp 9
 #define compressor_en 7
-inline uint8_t EN = 255;
+inline uint8_t EN = 0;
 
 #define start_speed 75
 #define end_speed 255
@@ -15,8 +15,8 @@ inline uint8_t EN = 255;
 #define step_size ((end_speed - start_speed) / (steps - 1))
 
 // Declare variables as `inline` to avoid multiple definition errors
-inline unsigned int speed = 0;
-inline unsigned int step = 0;
+inline uint8_t speed = 0;
+inline uint8_t step = 0;
 
 inline float c, ci, co, ei, eo, lf, rf, et, eh, ct, ch, pi, po;
 
